d76e9a4c5e0e9e80f41fb33b74179f1d0965114f
rt-snz <vacuousworld@gmail.com>
2018-10-24 10:59:52 +0900

    second commit

--- getProductId/index.js
+++ getProductId/index.js
@@ -1,4 +1,5 @@
 module.exports = async function (context, req) {
+    //git test comment
     var productId = req.query.productId;
     context.res = {
         body: "The product name for your product id " + productId  + " is Starfruit Explosion and the description is This starfruit ice cream is out of this world!"
